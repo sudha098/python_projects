5530d7802b168dfba2698adb27b2ca8044644909
Sudha Verma <external.sudha.verma@kmon.net>
2022-11-23 23:35:13 +0530

    added operators file

---
+++ arithmatic_operator.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(a % b)
+
+print(5 ** 4)  # 5*5*5*5
+print(5 ** 2)  # 5*5
+
+print(10 // 3)
+print(16 // 3)
---
+++ logical_operator.py
@@ -0,0 +1,7 @@
+x = 10
+y = 20
+print(x == 10 and y == 20 and x != y)
+
+print(x == 10 or y != 20 or x == y)
+
+print(not x!=10)
