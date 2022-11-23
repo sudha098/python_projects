812138f5db3dda884d34530dc437106f2cff7980
Sudha Verma <external.sudha.verma@kmon.net>
2022-11-24 00:10:00 +0530

    added bitwise operator

---
+++ bitwise_operator.py
@@ -0,0 +1,12 @@
+# true table
+x = 10
+y = 8
+
+print(bin(x)) #binary of x
+print(bin(y)) #binary of y
+
+print(x & y) #1000
+print(bin(x&y),x&y) #binary of x&y
+print(x | y) #1010
+print(x ^ y) #0010
+
