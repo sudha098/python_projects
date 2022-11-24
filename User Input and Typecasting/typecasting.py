ec8667fd858d564d2121c57d64ac6b1c0a7620f8
Sudha Verma <external.sudha.verma@kmon.net>
2022-11-24 18:44:11 +0530

    Added user input and typecasting

---
+++ User Input and Typecasting/typecasting.py
@@ -0,0 +1,20 @@
+number1 = int(input("Enter number1:"))
+number2 = int(input("Enter number2:"))
+
+print(number2 + number1)
+print(number2)
+print(number1)
+
+number1 = float(input("Enter number1:"))
+number2 = float(input("Enter number2:"))
+
+print(number2 + number1)
+print(number2)
+print(number1)
+
+number1 = eval(input("Enter number1:"))
+number2 = eval(input("Enter number2:"))
+
+print(number2 + number1)
+print(number2)
+print(number1)
---
+++ User Input and Typecasting/user_input.py
@@ -0,0 +1,8 @@
+
+
+name=input("Enter your name:" )
+item=input("Enter item name:")
+
+print(name+item)
+print(name)
+print(item)
