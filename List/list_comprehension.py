5af26d346fea5067e32ea628596373efb85bdeba
Sudha Verma <external.sudha.verma@kmon.net>
2022-11-25 18:13:17 +0530

    added list files

---
+++ List/list_comprehension.py
@@ -0,0 +1,16 @@
+mylist = []
+
+for i in range(1, 101):
+    mylist.append(i)
+print(mylist)
+
+mylist = [i for i in range(1, 101)]
+print(mylist)
+
+mylist = [i for i in range(1, 101) if i % 2 == 0]
+print(mylist)
+
+name="sudhaverma"
+mylist=[i for i in name]
+print(mylist)   
+
